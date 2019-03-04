35b59399afe426ecfc52e73e557fee593d454531
Wamiq <wamiqurehman093@gmail.com>
2019-03-04 14:06:34 +0500

    walls removed (works fine)

--- pakman.py
+++ pakman.py
@@ -13,29 +13,25 @@ BLUE = arcade.color.ALICE_BLUE
 
 ENTITY_SCALE = 0.3
 SPEED = 5
-WALL_SCALE = 0.5
 
 ENEMY_TOTAL = 5
-ENEMY_SCALE = 0.1
-ENEMY_SPEED = 2
+ENEMY_SCALE = 0.3
+ENEMY_SPEED = 3
 
 LEFT = arcade.key.LEFT
 RIGHT = arcade.key.RIGHT
 UP = arcade.key.UP
 DOWN = arcade.key.DOWN
 
-TOTAL_COINS = 200
+TOTAL_COINS = 100
 SCALE_COIN = 0.15
 
 class Enemy(arcade.Sprite):
-    def set_physics_engine(self, wall_list):
-        self.physics_engine = arcade.PhysicsEngineSimple(self, wall_list)
-
     def follow(self, player):
         self.center_x += self.change_x
         self.center_y += self.change_y
 
-        if random.randrange(100) == 0:
+        if random.randrange(25) == 0:
             start_x = self.center_x
             start_y = self.center_y
 
@@ -59,11 +55,9 @@ class Window(arcade.Window):
 
         self.gameover = False
         self.coin_list = None
-        self.wall_list = None
         self.enemy_list = None
         self.player = None
         self.physics_engine = None
-        self.enemy_physics_engine = None
         self.score = None
 
     def draw_hud(self):
@@ -73,7 +67,6 @@ class Window(arcade.Window):
     def on_draw(self):
         arcade.start_render()
         self.player.draw()
-        self.wall_list.draw()
         self.coin_list.draw()
         self.draw_hud()
         self.enemy_list.draw()
@@ -95,12 +88,11 @@ class Window(arcade.Window):
             self.player.change_y = -SPEED
 
     def update(self, delta_time):
-        self.physics_engine.update()
+        self.player.center_x += self.player.change_x
+        self.player.center_y += self.player.change_y
 
         for enemy in self.enemy_list:
             enemy.follow(self.player)
-        for enemy in self.enemy_list:
-            enemy.physics_engine.update()
 
         player_enemy = arcade.check_for_collision_with_list(self.player, self.enemy_list)
 
@@ -114,7 +106,6 @@ class Window(arcade.Window):
             self.score += 10
 
     def setup(self):
-        self.wall_list = arcade.SpriteList()
         self.coin_list = arcade.SpriteList()
         self.enemy_list = arcade.SpriteList()
 
@@ -122,59 +113,27 @@ class Window(arcade.Window):
         self.gameover = False
 
         self.player = arcade.Sprite('assets/images/pakman.png', ENTITY_SCALE)
-        self.player.center_x = 93
-        self.player.center_y = 93
+        self.player.center_x = 100
+        self.player.center_y = 100
 
-        for i in range(5):
+        for i in range(ENEMY_TOTAL):
             enemy = Enemy('assets/images/enemy.png', ENTITY_SCALE)
-            enemy_placed = False
-            enemy.set_physics_engine(self.wall_list)
-
-            while not enemy_placed:
-                enemy.center_x = random.randrange(100, WIDTH - 100)
-                enemy.center_y = random.randrange(100, HEIGHT - 100)
-
-                enemy_wall = arcade.check_for_collision_with_list(enemy, self.wall_list)
-
-                if len(enemy_wall) == 0:
-                    enemy_placed = True
+            enemy.center_x = random.randrange(50, WIDTH - 50)
+            enemy.center_y = random.randrange(50, HEIGHT- 50)
             self.enemy_list.append(enemy)
-
-        for y in range(31, HEIGHT - 31, HEIGHT - 63):
-            for x in range(30, WIDTH, 62):
-                wall = arcade.Sprite('assets/images/box.png', WALL_SCALE)
-                wall.center_x = x
-                wall.center_y = y
-                self.wall_list.append(wall)
-
-        for x in range(30, WIDTH, WIDTH - 62):
-            for y in range(93, HEIGHT, 63):
-                wall = arcade.Sprite('assets/images/box.png', WALL_SCALE)
-                wall.center_x = x
-                wall.center_y = y
-                self.wall_list.append(wall)
 
         for i in range(TOTAL_COINS):
             coin = arcade.Sprite('assets/images/coin.png', SCALE_COIN)
             coin_placed = False
 
             while not coin_placed:
-                coin.center_x = random.randrange(WIDTH)
-                coin.center_y = random.randrange(HEIGHT)
-
+                coin.center_x = random.randrange(5, WIDTH - 5)
+                coin.center_y = random.randrange(5, HEIGHT- 5)
                 coin_hitlist = arcade.check_for_collision_with_list(coin, self.coin_list)
-                wall_hitlist = arcade.check_for_collision_with_list(coin, self.wall_list)
-
-                if len(coin_hitlist) == 0 and len(wall_hitlist) == 0:
+                if len(coin_hitlist) == 0:
                     coin_placed = True
-
             self.coin_list.append(coin)
-
-
-        self.physics_engine = arcade.PhysicsEngineSimple(self.player, self.wall_list)
-
-        arcade.set_background_color(BLUE)
-
+        arcade.set_background_color(BLACK)
 
 def main():
     window = Window(WIDTH, HEIGHT, TITLE)
